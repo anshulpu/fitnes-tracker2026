63d389e7ccfa6e0cbd19133fefbd6a02fef62ed4
anshulpu <anshuyadav910280@gmail.com>
2026-05-13 23:31:37 +0530

    Connect to Supabase project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,6 +10,6 @@ export const environment = {
     appId: 'YOUR_PROD_APP_ID'
   },
   apiBaseUrl: 'https://fitness-tracker-api-gk3i.onrender.com',
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY'
+  supabaseUrl: 'https://bocsuatdeumbithkdfqv.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvY3N1YXRkZXVtYml0aGtkZnF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODk4MzMsImV4cCI6MjA5NDI2NTgzM30.0JKCqqD7rCBU-imEhHBCLm3RaHMmhEnJsStSV_zNSgA'
 };
